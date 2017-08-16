4d5a0c7744ca449f0549294b8c590f97b5945a0e
Aluno <Aluno@lab7pc11>
2017-08-16 13:16:41 -0300

    Pagina inicial do site

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,21 +7,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class Home extends MY_Controller {
     
+    protected $_template_folder = 'site';
+    protected $_data = array();
+    
     public function __construct() {
         parent::__construct();
-        $this->load->helper('theme');
     }
     
     public function index()
     {
-        $dados['msg'] = get_alert_code($this->session->flashdata('msg'), 'danger');
-        $dados['erros'] = $this->session->flashdata('erros');
-        $dados['usuario'] = $this->session->flashdata('dadosusuario');
-        $dados['abrir'] = $this->session->flashdata('abrir');
-        //die($dados['abrir']);
-        $this->view('home', $dados);
+        $this->view('home', $this->_data);
     }
-    
-    
-        
 }
