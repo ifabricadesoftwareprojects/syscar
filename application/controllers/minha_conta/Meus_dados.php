0caf7cf3f8303af66c6702d5dcfd405eea6a3789
Aluno <Aluno@lab7pc11>
2017-09-21 12:16:59 -0300

    Listando os dados da concessionaria em "Meus Dados"

--- application/controllers/minha_conta/Meus_dados.php
+++ application/controllers/minha_conta/Meus_dados.php
@@ -13,10 +13,16 @@ class Meus_dados extends MY_Controller {
         if(!$this->session->token){
             redirect('');
         }
+        $this->load->model('usuario_model', 'usuario');
     }
     
     public function index()
     {
+        $usuario = $this->usuario->findBy('token', $this->session->token);
+        //Apesar de a busca pelo token retornar apenas 1 registro, o método findBy sempre
+        //retorna um array de objetos. Nesse caso, ele retorna o array com apenas 1 posicao (Pos 0)
+        $this->_data['usuario'] = $usuario[0];
+        $this->_data['msg'] = $this->session->flashdata('msg');
         $this->view('meus_dados', $this->_data);
     }
 }
--- application/views/site/meus_dados_view.php
+++ application/views/site/meus_dados_view.php
@@ -30,74 +30,25 @@
         
       <div class="col-md-6 col-sm-8">
         <div class="profile_wrap">
-          <h5 class="uppercase underline">Meus Carros <span>(20 Cars)</span></h5>
-          <div class="my_vehicles_list">
-            <ul class="vehicle_listing">
-              <li>
-                <div class="vehicle_img"> <a href="#"><img src="<?php echo link_assets('site/images/recent-car-1.jpg') ?>" alt="image"></a> </div>
-                <div class="vehicle_title">
-                  <h6><a href="#">Mazda CX-5 SX, V6, ABS, Sunroof </a></h6>
-                </div>
-                <div class="vehicle_status"> <a href="#" class="btn outline btn-xs active-btn">Active</a>
-                  <div class="clearfix"></div>
-                  <a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-trash" aria-hidden="true"></i></a> </div>
-              </li>
-              <li class="deactive_vehicle">
-                <div class="vehicle_img"> <a href="#"><img src="<?php echo link_assets('site/images/recent-car-2.jpg') ?>" alt="image"></a> </div>
-                <div class="vehicle_title">
-                  <h6><a href="#">Mazda CX-5 SX, V6, ABS, Sunroof </a></h6>
-                </div>
-                <div class="vehicle_status"> <a href="#" class="btn outline btn-xs">Deactive</a>
-                  <div class="clearfix"></div>
-                  <a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-trash" aria-hidden="true"></i></a> </div>
-              </li>
-              <li>
-                <div class="vehicle_img"> <a href="#"><img src="<?php echo link_assets('site/images/recent-car-3.jpg') ?>" alt="image"></a> </div>
-                <div class="vehicle_title">
-                  <h6><a href="#">Ford Mustang 2.3 Ecoboost Premium </a></h6>
-                </div>
-                <div class="vehicle_status"> <a href="#" class="btn outline btn-xs active-btn">Active</a>
-                  <div class="clearfix"></div>
-                  <a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-trash" aria-hidden="true"></i></a> </div>
-              </li>
-              <li>
-                <div class="vehicle_img"> <a href="#"><img src="<?php echo link_assets('site/images/recent-car-4.jpg') ?>" alt="image"></a> </div>
-                <div class="vehicle_title">
-                  <h6><a href="#">Mazda CX-5 SX, V6, ABS, Sunroof </a></h6>
-                </div>
-                <div class="vehicle_status"> <a href="#" class="btn outline btn-xs active-btn">Active</a>
-                  <div class="clearfix"></div>
-                  <a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-trash" aria-hidden="true"></i></a> </div>
-              </li>
-              <li>
-                <div class="vehicle_img"> <a href="#"><img src="<?php echo link_assets('site/images/recent-car-5.jpg') ?>" alt="image"></a> </div>
-                <div class="vehicle_title">
-                  <h6><a href="#">Mazda CX-5 SX, V6, ABS, Sunroof </a></h6>
-                </div>
-                <div class="vehicle_status"> <a href="#" class="btn outline btn-xs active-btn">Active</a>
-                  <div class="clearfix"></div>
-                  <a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-trash" aria-hidden="true"></i></a> </div>
-              </li>
-              <li>
-                <div class="vehicle_img"> <a href="#"><img src="<?php echo link_assets('site/images/recent-car-3.jpg') ?>" alt="image"></a> </div>
-                <div class="vehicle_title">
-                  <h6><a href="#">Ford Mustang 2.3 Ecoboost Premium </a></h6>
-                </div>
-                <div class="vehicle_status"> <a href="#" class="btn outline btn-xs active-btn">Active</a>
-                  <div class="clearfix"></div>
-                  <a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-trash" aria-hidden="true"></i></a> </div>
-              </li>
-            </ul>
-            <div class="pagination">
-              <ul>
-                <li class="current">1</li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#">5</a></li>
-              </ul>
+          <h5 class="uppercase underline">Meus Dados</h5>
+          <p><?php echo $msg ?></p>
+          <form method="post">
+            <div class="form-group">
+                <label>Nome</label>
+                <input type="text" name="nome" class="form-control" placeholder="Nome" value="<?php echo $usuario->nome ?>">
             </div>
-          </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" class="form-control" placeholder="Email" value="<?php echo $usuario->email ?>">
+            </div>
+            <div class="form-group">
+                <label>Perfil</label>
+                <input type="text" name="perfil" class="form-control" placeholder="Perfil" value="<?php echo $usuario->perfil ?>" readonly>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn">Atualizar Dados <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
