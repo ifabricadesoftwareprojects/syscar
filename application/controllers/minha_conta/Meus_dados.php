5ad231e45dc2282a7195540df866944522516b63
Aluno <Aluno@lab7pc12>
2017-09-27 14:35:59 -0300

    Atualizar meus dados

--- application/controllers/minha_conta/Meus_dados.php
+++ application/controllers/minha_conta/Meus_dados.php
@@ -23,6 +23,39 @@ class Meus_dados extends MY_Controller {
         //retorna um array de objetos. Nesse caso, ele retorna o array com apenas 1 posicao (Pos 0)
         $this->_data['usuario'] = $usuario[0];
         $this->_data['msg'] = $this->session->flashdata('msg');
+        $this->_data['abrir'] = $this->session->flashdata('abrir');
+        $this->_data['alert_message'] = alert_message($this->session->flashdata('msg'), $this->session->flashdata('msgstatus'));
+        $this->_data['erros'] = $this->session->flashdata('erro');
+        $this->_data['dados_usuario'] = $this->session->flashdata('dados'); 
         $this->view('meus_dados', $this->_data);
+        
+    }
+    public function editar()
+    { 
+        if($this->input->post()){
+            $this->usar = $this->usuario->get_id_by_token($this->session->token);
+            $this->usuario = $this->usuario->find($this->usar);
+            $this->usuario = $this->usuario->post_to($this->input->post(), $this->usuario);
+            try{
+                $this->usuario->update('idusuario', $this->usuario->idusuario, $this->input->post('confirmar'));
+                $this->session->set_flashdata('msg', 'Usuario atualizado com sucesso');
+                $this->session->set_flashdata('msgstatus', 'success');
+                redirect('home');
+            } catch (Exception $ex) {
+                $this->session->set_flashdata('abrir', 'Usuario');
+                $this->session->set_flashdata('msg', 'Erro ao atualizar usuario:');
+                $this->session->set_flashdata('erro', $this->usuario->get_erro());
+                $this->session->set_flashdata('dados', $this->input->post());
+                $this->session->set_flashdata('msgstatus', 'error');
+            }
+        }
+        $this->_data['usuario'] = $this->usuario->find($id);
+        
+        $this->_data['sub_title'] = 'Editar usuario';
+        $this->_data['action'] = 'Atualizar';
+        $this->_data['alert_message'] = alert_message($this->session->flashdata('msg'), $this->session->flashdata('msgstatus'));
+        $this->_data['erros'] = $this->session->flashdata('erro');
+        $this->_data['dados_usuario'] = $this->session->flashdata('dados');
+        $this->view('home', $this->_data);
     }
 }
--- application/views/site/meus_dados_view.php
+++ application/views/site/meus_dados_view.php
@@ -1,57 +1,73 @@
 <!--my-vehicles-->
 <section class="user_profile inner_pages">
-  <div class="container">
-    <div class="user_profile_info gray-bg padding_4x4_40">
-      <div class="upload_user_logo"> <img src="<?php echo link_assets('site/images/dealer-logo.jpg') ?>" alt="image">
-        <div class="upload_newlogo">
-          <input name="upload" type="file">
+    <div class="container">
+        <div class="user_profile_info gray-bg padding_4x4_40">
+            <div class="upload_user_logo"> <img src="<?php echo link_assets('site/images/dealer-logo.jpg') ?>" alt="image">
+                <div class="upload_newlogo">
+                    <input name="upload" type="file">
+                </div>
+            </div>
+            <div class="dealer_info">
+                <h5>Autospot Used Cars Center </h5>
+                <p>P.1225 N Broadway Ave <br>
+                    Oklahoma City, OK  1234-5678-090</p>
+            </div>
         </div>
-      </div>
-      <div class="dealer_info">
-        <h5>Autospot Used Cars Center </h5>
-        <p>P.1225 N Broadway Ave <br>
-          Oklahoma City, OK  1234-5678-090</p>
-      </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-3">
+                <?php if ($this->session->token) : ?>  
+                    <div class="profile_nav user_login">
+                        <ul>
+                            <li class="active"><a href="<?php echo base_url('minha_conta/meus_dados') ?>">MEUS DADOS </a></li>
+                            <li><a href="<?php echo base_url('minha_conta/meus_veiculos') ?>">MEUS VEÍCULOS</a></li>
+                            <li><a href="<?php echo base_url('minha_conta/meus_veiculos/adicionar') ?>">ADICONAR NOVO VEÍCULO</a></li>
+                            <li><a href="<?php echo base_url('minha_conta/logout') ?>">SAIR</a></li>
+                        </ul>
+                    </div>
+                <?php endif; ?> 
+            </div>
+
+
+            <div class="col-md-6 col-sm-8">
+                <div class="profile_wrap">
+                    <h5 class="uppercase underline">Meus Dados</h5>
+                    <p><?php echo $msg ?></p>
+                    <form action="<?php echo base_url('minha_conta/meus_dados/editar') ?>" method="post">
+                        <div class="form-group">
+                            <label>Nome</label>
+                            <input type="text" name="nome" class="form-control" placeholder="Nome" value="<?php echo $usuario->nome ?>"<?php echo (isset($dados_usuario) ? $dados_usuario['nome'] : '') ?>>
+                            <?php echo display_erros(isset($erros['nome']) ? $erros['nome'] : null) ?>
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="text" name="email" class="form-control" placeholder="Email" value="<?php echo $usuario->email ?>"<?php echo (isset($dados_usuario) ? $dados_usuario['email'] : '') ?>>
+                            <?php echo display_erros(isset($erros['email']) ? $erros['email'] : null) ?>
+                        </div>
+                        <div class="form-group">
+                            <label for="perfil">Perfil:</label>
+                            <select name="perfil" class="form-control ">
+                                <option value="Usuario"<?php echo $usuario->perfil == 'Usuario' ? ' selected' : '' ?>>Usuario</option>
+                                <option value="Concessionaria"<?php echo $usuario->perfil == 'Concessionaria' ? ' selected' : '' ?>>Concessionária</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Senha</label>
+                            <input type="password" name="senha" class="form-control" placeholder="" value=""<?php echo (isset($dados_usuario) ? $dados_usuario['senha'] : '') ?>>
+                            <?php echo display_erros(isset($erros['senha']) ? $erros['senha'] : null) ?>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Confirmar Senha</label>
+                            <input type="password" name="confirmar" class="form-control" placeholder="" value=""<?php echo (isset($dados_usuario) ? $dados_usuario['confirmar'] : '') ?>>
+                            <?php echo display_erros(isset($erros['confirmar']) ? $erros['confirmar'] : null) ?>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn">Atualizar Dados <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="row">
-      <div class="col-md-3 col-sm-3">
-         <?php if ($this->session->token) : ?>  
-        <div class="profile_nav user_login">
-          <ul>
-            <li class="active"><a href="<?php echo base_url('minha_conta/meus_dados') ?>">MEUS DADOS </a></li>
-            <li><a href="<?php echo base_url('minha_conta/meus_veiculos') ?>">MEUS VEÍCULOS</a></li>
-            <li><a href="<?php echo base_url('minha_conta/meus_veiculos/adicionar') ?>">ADICONAR NOVO VEÍCULO</a></li>
-            <li><a href="<?php echo base_url('minha_conta/logout') ?>">SAIR</a></li>
-          </ul>
-        </div>
-        <?php endif; ?> 
-      </div>
-        
-        
-      <div class="col-md-6 col-sm-8">
-        <div class="profile_wrap">
-          <h5 class="uppercase underline">Meus Dados</h5>
-          <p><?php echo $msg ?></p>
-          <form method="post">
-            <div class="form-group">
-                <label>Nome</label>
-                <input type="text" name="nome" class="form-control" placeholder="Nome" value="<?php echo $usuario->nome ?>">
-            </div>
-            <div class="form-group">
-                <label>Email</label>
-                <input type="text" name="email" class="form-control" placeholder="Email" value="<?php echo $usuario->email ?>">
-            </div>
-            <div class="form-group">
-                <label>Perfil</label>
-                <input type="text" name="perfil" class="form-control" placeholder="Perfil" value="<?php echo $usuario->perfil ?>" readonly>
-            </div>
-            <div class="form-group">
-              <button type="submit" class="btn">Atualizar Dados <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
 </section>
 <!--/my-vehicles--> 
